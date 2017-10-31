7931d9b520048b94181c154a0b428977d721bc76
Jakub Wróblewski <j.wroblewski97@gmail.com>
2017-10-31 16:31:19 +0100

    Fixed? v2

--- tests.cpp
+++ tests.cpp
@@ -9,7 +9,7 @@ int main() {
     unsigned long id1 = dict_new();
     dict_insert(id1, "key", "value");
     const char* v = dict_find(id1, "key");
-    assert(string(v) == "value");
+    assert(std::string(v) == "value");
     dict_delete(id1);
     assert(dict_size(id1) == 0);
 
